2ee0f274f9db1a6c3d9fac11160deaf1c5541677
Nick van der Raaf <nickraaf@outlook.com>
2023-10-16 14:59:26 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>Ik ontwikkel websites <br className="sm:block hidden" /> en webapplicaties</p>
         </div>
       </div>
-      <MeCanvas />
+      {/* <MeCanvas /> */}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
